5edf98cab253b774a31c1abdc4a5e4ec8a6c9293
Birkin James Diana <birkin.diana@gmail.com>
2024-03-19 16:02:47 -0400

    creates-tables and adds-records for location-testing.

--- b__add_records.py
+++ b__add_records.py
@@ -1,15 +1,24 @@
 """
-Adds two Citation records, and for each of those, adds two CitationField records.
-Can be run multiple times to add more records.
+Depending on arguments, adds citation-data or location-data to the database
 
-Usage:
+location-data usage:
 - cd /to/this/git-directory/
 - source ../env/bin/activate
-- python ./b__add_records.py
+- python ./b__add_records.py --arg location_stuff
+
+citationd-data usage:
+- cd /to/this/git-directory/
+- source ../env/bin/activate
+- python ./b__add_records.py --arg citation_stuff
+
+Adds two Citation records, and for each of those, adds two CitationField records.
+Can be run multiple times to add more records.
 """
 
+import argparse
 import models_sqlalchemy
 from models_sqlalchemy import Citation, CitationField
+from models_sqlalchemy import Reference, Location, ReferenceLocation  # ReferenceLocation is the target '5_has_location' table
 
 
 ## make session
@@ -17,31 +26,88 @@ session = models_sqlalchemy.make_session()
 assert session is not None
 
 
-## add two Citation records
-citation1 = Citation(comments='Dummy comment 1', acknowledgements='Dummy acknowledgement 1')
-citation2 = Citation(comments='Dummy comment 2', acknowledgements='Dummy acknowledgement 2')
+def add_location_data():
 
-session.add(citation1)
-session.add(citation2)
+    ## add a Reference record
+    reference1 = Reference( 
+        id='1', 
+        transcription='the transcription' 
+        )
+    session.add(reference1)
 
-## commit citations so their ids are available
-session.commit()
+    ## commit reference so id is available
+    session.commit()
 
-# Add two CitationField records for each Citation
-citation_field1 = CitationField( citation_id=citation1.id, field_data='Dummy field data 1.1' )
-citation_field2 = CitationField( citation_id=citation1.id, field_data='Dummy field data 1.2' )
-citation_field3 = CitationField( citation_id=citation2.id, field_data='Dummy field data 2.1' )
-citation_field4 = CitationField( citation_id=citation2.id, field_data='Dummy field data 2.2' )
+    ## add a Location record
+    location1 = Location( 
+        id='1', 
+        name='Location01' 
+        )
+    session.add(location1)
 
-session.add(citation_field1)
-session.add(citation_field2)
-session.add(citation_field3)
-session.add(citation_field4)
+    ## commit location so id is available
+    session.commit()
 
-# Commit the changes to the database
-session.commit()
+    # Add one ReferenceLocation record
+    reference_location1 = ReferenceLocation( 
+        id='1',
+        reference_id=reference1.id, 
+        location_id=location1.id 
+        )
 
-# Close the session
-session.close()
+    session.add(reference_location1)
 
-print('Records added successfully.')
+    ## Commit changes
+    session.commit()
+
+    ## Close session
+    session.close()
+
+    print('location-data added successfully.')
+
+
+def add_citation_data():
+
+    ## add two Citation records
+    citation1 = Citation(comments='Dummy comment 1', acknowledgements='Dummy acknowledgement 1')
+    citation2 = Citation(comments='Dummy comment 2', acknowledgements='Dummy acknowledgement 2')
+
+    session.add(citation1)
+    session.add(citation2)
+
+    ## commit citations so their ids are available
+    session.commit()
+
+    # Add two CitationField records for each Citation
+    citation_field1 = CitationField( citation_id=citation1.id, field_data='Dummy field data 1.1' )
+    citation_field2 = CitationField( citation_id=citation1.id, field_data='Dummy field data 1.2' )
+    citation_field3 = CitationField( citation_id=citation2.id, field_data='Dummy field data 2.1' )
+    citation_field4 = CitationField( citation_id=citation2.id, field_data='Dummy field data 2.2' )
+
+    session.add(citation_field1)
+    session.add(citation_field2)
+    session.add(citation_field3)
+    session.add(citation_field4)
+
+    ## Commit changes 
+    session.commit()
+
+    ## Close session
+    session.close()
+
+    print('citation-data added successfully.')
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser( description='Process some arguments.' )
+    parser.add_argument( '--arg', required=True, type=str, help='Argument to decide which function to call' )
+    ## list args -----------------------------------
+    args = parser.parse_args()
+    print( f'- args, ``{args}``' )
+    ## handle args ------------------------------
+    if args.arg == 'citation_data':
+        add_citation_data()
+    elif args.arg == 'location_data':
+        add_location_data()
+    else:
+        print( f'- No function matches the argument: {args.arg}' )
--- models_sqlalchemy.py
+++ models_sqlalchemy.py
@@ -61,6 +61,9 @@ def make_session():
 ## END session configuration ----------------------------------------
 
 
+## citation data start ----------------------------------------------
+
+
 class Citation( Base ):
     __tablename__ = '3_citations'
 
@@ -81,6 +84,160 @@ class CitationField( Base ):
         primaryjoin=(citation_id == Citation.id),
         backref='citation_data')
     field_data = Column(String(255))
+
+
+## citation data end ------------------------------------------------
+    
+
+## reference data start ---------------------------------------------
+
+
+class Reference( Base ):
+    __tablename__ = '4_references'
+
+    id = Column(Integer, primary_key=True)
+    transcription = Column(UnicodeText())
+
+    def __repr__(self):
+        return '<Reference {0}>'.format(self.id)
+    
+
+class Location(Base):
+    __tablename__ = '1_locations'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(255))
+    # origin_for = relationship('Referent',
+    #     secondary=has_origin, back_populates='origins')
+
+    def __repr__(self):
+        return '<Location {0}: {1}>'.format(self.id, self.name)
+
+
+class ReferenceLocation( Base ):
+    __tablename__ = '5_has_location'
+
+    id = Column(Integer, primary_key=True)
+    reference_id = Column(Integer, ForeignKey('4_references.id'))
+    reference = relationship(Reference,
+        primaryjoin=(reference_id == Reference.id),
+        backref='locations')
+    location_id = Column(Integer, ForeignKey('1_locations.id'))
+    location = relationship(Location,
+        primaryjoin=(location_id == Location.id),
+        backref='references')
+    # location_type_id = Column(Integer, ForeignKey('1_location_types.id'))
+    # location_rank = Column(Integer)
+
+
+## reference data end -----------------------------------------------
+
+
+## for reference ----------------------------------------------------
+    
+# class Reference(Base):
+#     __tablename__ = '4_references'
+
+#     id = Column(Integer, primary_key=True)
+#     citation_id = Column(Integer, ForeignKey('3_citations.id'),
+#         nullable=False)
+#     reference_type_id = Column(Integer, ForeignKey('1_reference_types.id'),
+#         nullable=False)
+#     national_context_id = Column(Integer, ForeignKey('1_national_context.id'),
+#         nullable=False)
+#     date = Column(DateTime())
+#     transcription = Column(UnicodeText())
+#     image_url = Column( String(500) )
+#     referents = relationship(
+#         'Referent', backref='reference', lazy=True, cascade="delete")
+#     groups = relationship(
+#         'Group', backref='reference', lazy=True, cascade="delete")
+
+#     def last_edit(self):
+#         """ Note: self.edits is possible because of ReferenceEdit() """
+#         edits: list[tuple] = sorted([ (e.timestamp, e) for e in self.edits ],
+#              key=operator.itemgetter(0), reverse=True)
+#         log.debug( f'edits, ```{edits}```' )
+#         if edits:
+#             return_edits = edits[0][1]
+#         else:
+#             return_edits = None
+#         log.debug( f'return_edits, ``{return_edits}``' )
+#         log.debug( f'type(return_edits), ``{type(return_edits)}``' )
+#         return return_edits
+
+#     def display_date(self):
+#         if self.date:
+#             return self.date.strftime('%Y %B %d')
+#         else:
+#             return ''
+
+#     def display_location_info( self ):
+#         # session = make_session()
+#         locations_lst = []
+#         rfrnc_locations = self.locations
+#         log.debug( f'rfrnc_locations, ```{rfrnc_locations}```' )
+#         for rfrnc_location in rfrnc_locations:
+#             location_name = rfrnc_location.location.name
+#             location_type = None
+#             try:
+#                 location_type = rfrnc_location.location_type.name
+#             except:
+#                 log.exception( f'problem parsing rfrnc_location, ```{rfrnc_location.__dict__}```; traceback follows; processing will continue' )
+#             loc_dct = { 'location_name': location_name, 'location_type': location_type }
+#             log.debug( f'loc_dct, ```{loc_dct}```' )
+#             locations_lst.append( loc_dct )
+#         return locations_lst
+
+#     def dictify( self ):
+#         if self.date:
+#             datetime_obj = cast( datetime.datetime, self.date )
+#             isodate = datetime.date.isoformat( datetime_obj )
+#         else:
+#             isodate = ''
+#         jsn_referents = []
+#         for rfrnt in self.referents:
+#             jsn_referents.append( {'id': rfrnt.id, 'age': rfrnt.age, 'sex': rfrnt.sex} )
+#         last_edit = self.last_edit()
+#         if last_edit:
+#             last_edit_str: str = last_edit.timestamp.strftime( '%Y-%m-%d' )
+#         else: 
+#             last_edit_str = ''
+#         data = {
+#             'id': self.id,
+#             'citation_id': self.citation_id,
+#             'reference_type_id': self.reference_type_id,
+#             'reference_type_name': self.reference_type.name,  # NB: this appears to be an sqlalchemy convention -- that if there is a ForeignKey, I can just go ahead and refernce the property name.
+#             'national_context_id': self.national_context_id,
+#             'date': isodate,
+#             'transcription': self.transcription,
+#             'referents': jsn_referents,
+#             'last_edit': last_edit_str,
+#             'location_info': self.display_location_info()
+#             }
+#         return data
+
+#     def __repr__(self):
+#         return '<Reference {0}>'.format(self.id)
+    
+#     ## end class Reference() ----------------------------------------
+
+
+# class ReferenceLocation(Base):
+#     __tablename__ = '5_has_location'
+
+#     id = Column(Integer, primary_key=True)
+#     reference_id = Column(Integer, ForeignKey('4_references.id'))
+#     location_id = Column(Integer, ForeignKey('1_locations.id'))
+#     location_type_id = Column(Integer, ForeignKey('1_location_types.id'))
+#     location_rank = Column(Integer)
+#     reference = relationship(Reference,
+#         primaryjoin=(reference_id == Reference.id),
+#         backref='locations')
+#     location = relationship(Location,
+#         primaryjoin=(location_id == Location.id),
+#         backref='references')
+
 
 
 # class CitationField(Base):
